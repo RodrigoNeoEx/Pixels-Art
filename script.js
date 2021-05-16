27cfb72e0875dbfaeaeadadf0090c91794c85c96
RodrigoNeoEx <draja.acc@gmail.com>
2021-05-16 13:00:17 -0300

    functions to set random colors without white

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function createPaletteSquares() {
     document.getElementById('color-palette').appendChild(squareColor);
   }
 }
+
+function createRandomRGBColor() {
+  const red = Math.floor(Math.random()*255);
+  const green = Math.floor(Math.random()*255);
+  const blue = Math.floor(Math.random()*255);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function setPaletteColors() {
+  let firstColor = document.querySelectorAll('.color')[1];
+  let secondColor = document.querySelectorAll('.color')[2];
+  const thirdColor = document.querySelectorAll('.color')[3];
+  firstColor.style.backgroundColor = createRandomRGBColor();
+  secondColor.style.backgroundColor = createRandomRGBColor();
+  thirdColor.style.backgroundColor = createRandomRGBColor();
+  if (firstColor = secondColor || thirdColor || rgb(255, 255, 255)) {
+    firstColor = createRandomRGBColor();
+  }
+  if (secondColor = thirdColor || rgb(255, 255, 255)) {
+    secondColor = createRandomRGBColor();
+  }
+}
